60feff029101ec5a62b09621655af873f0a85d2c
ConstantlyRecompiling <117422532+ConstantlyRecompiling@users.noreply.github.com>
2022-11-19 01:29:04 +1100

    Add files via upload

--- data_processing/data_avg2.py
+++ data_processing/data_avg2.py
@@ -11,14 +11,17 @@ def open_json_file(file_path):
 #Filter database to keep only the SSIDS we need
 def avg_filter_data(filename):
     locs = defaultdict(list)
+    locs_count = defaultdict(lambda: 0)
     new_locs = defaultdict(lambda: defaultdict(lambda: [0.0, 0]))
+    output = defaultdict(lambda: defaultdict(lambda: [0.0, 0]))
     database = open_json_file(filename)
     ssid_to_keep = ['eduroam', 'uniwide', 'UNSW Guest', 'Global_Students']
 
     #Loop through array of dictionaries
     for location in database:
-        for wifi_ap_entry_items in location.items():
-            locs[wifi_ap_entry_items[0]].append(wifi_ap_entry_items[1])
+        for loc, samples in location.items():
+            locs[loc].append(samples)
+            locs_count[loc] += 1
 
     for loc, samples in locs.items():
         for sample in samples:
@@ -29,13 +32,12 @@ def avg_filter_data(filename):
             
     for loc, sample in new_locs.items():
         for ssid_mac, rssi_tuple in sample.items():
-            if rssi_tuple[1] == 0:
-                del sample[ssid_mac]
-            else:
-                new_locs[loc][ssid_mac] = rssi_tuple[0] / rssi_tuple[1]
-    return new_locs
+            if not rssi_tuple[1] <= (locs_count[loc] / 2):
+                output[loc][ssid_mac] = rssi_tuple[0] / rssi_tuple[1]
+                
+    return output
 
 
 if __name__ == "__main__":
-    with open('avg_filter_data3.json', 'w') as fp:
+    with open('avg_filter_data6.json', 'w') as fp:
         json.dump(avg_filter_data("data2.json"), fp)
